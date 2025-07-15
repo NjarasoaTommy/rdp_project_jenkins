2506a6015060c78d56086740c5b9d58754e3f3c5
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-07-15 20:41:55 +0300

    Add possible routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tri-color-fire',
+    loadComponent: () =>
+      import('./tri-color-fire/tri-color-fire.component').then(
+        (m) => m.TriColorFireComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./tri-color-fire/tri-color-fire.component').then(
+        (m) => m.TriColorFireComponent
+      ),
+  },
+];
